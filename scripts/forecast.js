ee02c788fa607dbc6147ef40572310da47bd2c66
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-01-19 20:30:37 +0100

    weatehr informaiton from api added

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,19 @@
 const key = "G2cYlot1IUZRPjsxWtReRXxhQzb9GBsI";
 
+//get weather Information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
 //get city Information
 const getCity = async (city) => {
-  const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
@@ -12,5 +23,10 @@ const getCity = async (city) => {
 };
 
 getCity("manchester")
-  .then((data) => console.log(data))
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
   .catch((err) => console.log(err));
