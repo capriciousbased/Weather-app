10fbd8b5e3322c010ad787bc53afcf4ec75b8679
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-11-14 18:45:24 +0100

    update forecast file

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,10 +4,8 @@ const key = "G2cYlot1IUZRPjsxWtReRXxhQzb9GBsI";
 const getWeather = async (id) => {
   const base = "http://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
-
   const response = await fetch(base + query);
   const data = await response.json();
-
   return data[0];
 };
 
@@ -15,7 +13,6 @@ const getWeather = async (id) => {
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
-
   const response = await fetch(base + query);
   const data = await response.json();
 
